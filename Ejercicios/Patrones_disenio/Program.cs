021dbec43dd5a57749f402b2f2632f5b52e9b384
svasquez <svasquez@controldiseno.com>
2019-11-07 20:52:54 -0500

    Primer ejercicio de patron de estrategia

---
+++ Ejercicios/Patrones_disenio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patrones_disenio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
